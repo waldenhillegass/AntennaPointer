f288cd0412132feaa3a25ac98223a06b59c8b7f7
Walden Hillegass <waldenhillegass@gmail.com>
2021-04-29 17:34:02 -0700

    asd

--- navx.py
+++ navx.py
@@ -34,12 +34,12 @@ class NAVX:
    def getYaw(self):
       # sensor returns hundreths
       number = self.bus.read_byte_data(DEV_ADDR, YAW_LOW)
-      number = int.from_bytes(number, byteorder='little', signed=True)
+      #number = int.from_bytes(number, byteorder='little', signed=True)
       return number / 100
 
    def getPitch(self):
       number = self.bus.read_byte_data(DEV_ADDR, PITCH_LOW)
-      number = int.from_bytes(number, byteorder='little', signed=True)
+      #number = int.from_bytes(number, byteorder='little', signed=True)
       return number / 100
 
    def __str__(self):
