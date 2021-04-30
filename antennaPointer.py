c4f83297b288d6d8b950fcc6b84896ac0ecfba02
Walden Hillegass <waldenhillegass@gmail.com>
2021-04-29 23:00:08 -0700

    dfdf

--- antennaPointer.py
+++ antennaPointer.py
@@ -1,11 +1,29 @@
 from navx import NAVX
 from time import sleep
+import serial
 
 imu = NAVX(1);
 
+def set_up_gps():
+     ser = serial.Serial(
+        port = '/dev/ttyAMA0',
+        baudrate = 9600,
+        parity = serial.PARITY_NONE,
+        stopbits = serial.STOPBITS_ONE,
+        bytesize = serial.EIGHTBITS,
+        timeout=1
+        )
+     counter=0
+     return ser
+gps = set_up_gps()
 
 while(True):
    sleep(1)
    print(imu)
+
+   print(gps.read_until())
+
+  
+   
 
 
