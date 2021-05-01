e251592b65db5e1f16cc1fae735b360c8a596e4f
Walden Hillegass <waldenhillegass@gmail.com>
2021-05-01 12:33:20 -0700

    asdasd

--- ledtest.py
+++ ledtest.py
@@ -4,6 +4,8 @@ pixels = neopixel.NeoPixel(board.D18, 30)
 
 while(True):
    pixels[0] = (255, 0, 0)
-   sleep(30)
+   pixels.show()
+   sleep(3)
    pixels.fill((0,128,0))
-   sleep(30)
+   pixels.show()
+   sleep(3)
