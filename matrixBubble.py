e9e8ebc6ae244ef5e5c1ce710416d2050a06e764
Walden Hillegass <waldenhillegass@gmail.com>
2021-05-02 20:25:36 -0700

    poop

--- matrixBubble.py
+++ matrixBubble.py
@@ -21,7 +21,7 @@ class Matrix:
       print(f'Y: {y}')
 
 
-      graph[1][0] = True
+      graph[5][0] = True
       #graph[x + 1][y] = True
       #graph[x][y + 1] = True
       #graph[x + 1][y + 1] = True
