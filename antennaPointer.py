00e3022bc5a2b2cf64e27a73a107be20e31f84b2
Walden Hillegass <waldenhillegass@gmail.com>
2021-05-05 18:38:14 -0700

    asdasd

--- antennaPointer.py
+++ antennaPointer.py
@@ -2,9 +2,10 @@ from navx import NAVX
 from gps import GPS
 from matrixBubble import Matrix
 from time import sleep
+from imu import IMU
 import serial
 
-imu = NAVX(1);
+imu = IMU();
 gps = GPS()
 
 scale = int(input("Enter scale: "))
@@ -20,3 +21,9 @@ while(True):
    x = float(input("Enter X error:"))
    y = float(input("Enter Y error:"))
    matrix.updateFromErrors(x,y)
+
+
+# Must be in degrees and elev in meters
+
+
+
