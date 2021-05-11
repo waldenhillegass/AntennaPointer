5536b73104d9fc6066ae857f9ab4a1451dd064c2
Walden Hillegass <waldenhillegass@gmail.com>
2021-05-11 14:22:14 -0700

    asdasd

--- antennaPointer.py
+++ antennaPointer.py
@@ -3,52 +3,70 @@ from matrixBubble import Matrix
 from time import sleep
 from imu import IMU
 from calculations import *
+from time import gmtime, strftime
+import sqlite3
 import serial
+from sqlite3 import Error
+import json
+import logging
 
-tPosLat = 35.3025
-tPosLong = -120.6974
-tPosElv = 471
+def main():
+   imu = IMU()
+   gps = GPS()
 
-imu = IMU()
-gps = GPS()
-
-scale = int(input("Enter scale: "))
-matrix = Matrix(scale)
-
-
-while(True):
-   imu.printStatus()
-
-   gps.readGPS()
-   print(gps)
+   scale = int(input("Enter scale: "))
+   matrix = Matrix(scale)
 
 
-   sweepElv = calcAngles(gps.getLatitude(), gps.getLongitude(), gps.elevation, tPosLat, tPosLong, tPosElv)
+   while(True):
+      tPosLat, tPosLong, tPosElv = get_balloon_gps() 
 
-   try:
-      dx = sweepElv[0] - imu.sensor.euler[0]
-      dy = sweepElv[1] - imu.sensor.euler[2]
-      dx *= -1
+      imu.printStatus()
 
-      print(sweepElv)
-
-      if dx < -180:
-         dx += 360
-      if dx > 180:
-         dx -= 360
-      print(f"dy = {dy}")
-      print(f"dx = {dx}")
-
-   except Exception as e:
-      print("IMU READ ERROR")
-      print(e)
+      gps.readGPS()
+      print(gps)
 
 
-   matrix.updateFromErrors(dx,dy)
-   sleep(.1)
+      sweepElv = calcAngles(gps.getLatitude(), gps.getLongitude(), gps.elevation, tPosLat, tPosLong, tPosElv)
+
+      try:
+         dx = sweepElv[0] - imu.sensor.euler[0]
+         dy = sweepElv[1] - imu.sensor.euler[2]
+         dx *= -1
+
+         print(sweepElv)
+
+         if dx < -180:
+            dx += 360
+         if dx > 180:
+            dx -= 360
+         print(f"dy = {dy}")
+         print(f"dx = {dx}")
+
+      except Exception as e:
+         print("IMU READ ERROR")
+         print(e)
 
 
-# Must be in degrees and elev in meters
+      matrix.updateFromErrors(dx,dy)
+      sleep(.1)
+
+# Read data from serial
+def get_balloon_gps():
+   payload = ser.readline()
+   prstrip = payload.rstrip().decode('utf8')
+   if len(prstrip) > 0:
+      print(prstrip)
+      try:
+         p = json.loads(prstrip)
+      except:
+         print(prstrip)
+         print("Invalid Packet")
+         
+   return p['latitude'], p['longitude'], p['altitude']
+
+main()
+   # Must be in degrees and elev in meters
 
 
 
